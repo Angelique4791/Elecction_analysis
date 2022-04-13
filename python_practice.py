4ef1a563f2ee767d19bdcb6dcf12986e0d555b55
Angelique Walker <102499547+Angelique4791@users.noreply.github.com>
2022-04-13 00:03:19 -0500

    testing codefor election analysis

---
+++ python_practice.py
@@ -0,0 +1,16 @@
+from pkgutil import walk_packages
+
+
+print("Hello World!")
+print("Hello to you, too, Atom!")
+name = "Brian Johnson"
+country = "United States"
+age = 34
+hourly_wage = 12
+daily_wage = hourly_wage * 8
+satisfied = True
+print(f'Hello {name}')
+print(f'{name} is from {country}')
+print(f'{name} is {age} years old')
+print(f'{name} earned ${daily_wage} today')
+print (f'{name} is satisfied? {satisfied}')
